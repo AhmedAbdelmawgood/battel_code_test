8639aa84880367e87bc66fd8609c3dc346775816
Ahmed <mody.ahmed1350@yahoo.com>
2017-01-13 20:34:57 +0200

    Adding scours random moving

--- src/myfirstplayer/RobotPlayer.java
+++ src/myfirstplayer/RobotPlayer.java
@@ -40,7 +40,7 @@ public class RobotPlayer {
             			}
         				int fate = rand.nextInt(1000);
             			Direction dir = directions[fate%8];
-            			if (rc.senseRubble(rc.getLocation().add(dir)) >= GameConstants.RUBBLE_OBSTRUCTION_THRESH ){
+            			if (rc.senseRubble(rc.getLocation().add(dir)) >= GameConstants.RUBBLE_OBSTRUCTION_THRESH){
             				rc.clearRubble(dir);
             			}else if(rc.canMove(dir)){
             				rc.move(dir);
@@ -54,8 +54,70 @@ public class RobotPlayer {
         	}
             
         }
+        else if (rc.getType() == RobotType.SOLDIER){
+            try{}
+            catch(Exception e){
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+            }
+            while(true){
+                try{
+                    while(true){
+                        // if(senseEnemey(rc.getLocation(r))
+                    }
+                }
+                catch (Exception e){
+                    System.out.println(e.getMessage());
+                    e.printStackTrace();    
+                }  
+            }
+        }
+        else if (rc.getType() == RobotType.SCOUT){
+            try{}
+            catch(Exception e){System.out.println(e.getMessage());
+            e.printStackTrace();
+            }
+            while(true){
+                try{
+                    if (rc.isCoreReady()){
+                        if(isItDangerous() == false){
+                            RobotType typeToBuild = robotTypes[0];
+                            if (rc.hasBuildRequirements(typeToBuild)) {
+                                Direction dirToBuild = directions[rand.nextInt(8)];
+                                for (int i = 0; i < 8; i++) {
+                                    // If possible, build in this direction
+                                    if (rc.canBuild(dirToBuild, typeToBuild)) {
+                                        rc.build(dirToBuild, typeToBuild);
+                                        break;
+                                    } else {
+                                        // Rotate the direction to try
+                                        dirToBuild = dirToBuild.rotateLeft();
+                                    }
+                                }
+                            }//end of if has build requirements
+                        }
+                        else{
+                            
+                        }
+                        int fate = rand.nextInt(1000);
+                        Direction dir = directions[fate%8];
+                        if (rc.senseRubble(rc.getLocation().add(dir)) >= GameConstants.RUBBLE_OBSTRUCTION_THRESH){
+                            rc.clearRubble(dir);
+                        }else if(rc.canMove(dir)){
+                            rc.move(dir);
+                        }
+                    }
+                }catch (Exception e){
+                    System.out.println(e.getMessage());
+                    e.printStackTrace();    
+                }
+                Clock.yield();
+            }
+        }
 	}
 	public static boolean isItDangerous(){
 		return false;
 	}
+
 }
+
